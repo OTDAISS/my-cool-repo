1682959dc82b7136e2044fc5a87157555ee0b959
OTDAISURFERSURVIVOR <oceantidedrop@gmail.com>
2026-03-18 17:42:53 -0400

    Update index.js

--- api/index.js
+++ api/index.js
@@ -5,30 +5,26 @@ import { fileURLToPath } from 'url';
 const app = express();
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
+// We use '..' because index.js is now inside the /api folder
+app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
 
-// Middleware: Parses data and prevents the "hang"
 app.use(express.json());
 app.use((req, res, next) => {
-    console.log(`Survivor Path: ${req.url}`);
     next(); 
 });
 
-// Serve the CSS/JS/Images Vite creates
-app.use(express.static(path.join(__dirname, 'dist')));
+// Point static files to the root /dist folder
+app.use(express.static(path.join(__dirname, '../dist')));
 
 // --- ROUTES ---
-
-// 1. Home Page (EJS)
 app.get('/', (req, res) => {
-    res.render('index', { title: 'Surfer Survivor Home' });
+    res.render('index', { title: 'Surfer Survivor' });
 });
 
-// 2. Dashboards (React)
-// These routes send the main HTML file so React can show your .jsx components
+// React Dashboards (Pink & Blue)
 app.get(['/dashboard-pink', '/dashboard-blue'], (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+    res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
 export default app;
