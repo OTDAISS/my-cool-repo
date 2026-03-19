8548274b78f08de5a1c72773cf8783f46273f2e0
OTDAISURFERSURVIVOR <oceantidedrop@gmail.com>
2026-03-19 11:43:21 -0400

    Update index.js

--- api/index.js
+++ api/index.js
@@ -5,24 +5,22 @@ import { fileURLToPath } from 'url';
 const app = express();
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-// We use '..' because index.js is now inside the /api folder
+// Point to the root folders from inside /api
 app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'ejs');
 
 app.use(express.json());
-app.use((req, res, next) => {
-    next(); 
-});
-
-// Point static files to the root /dist folder
 app.use(express.static(path.join(__dirname, '../dist')));
 
-// --- ROUTES ---
+// --- SURFER ROUTES ---
+
+// Home Page
 app.get('/', (req, res) => {
-    res.render('index', { title: 'Surfer Survivor' });
+    res.render('index'); 
 });
 
-// React Dashboards (Pink & Blue)
+// Direct these URLs to the Vite-built React app
+// React will then look at src/pages/free.jsx or members.jsx
 app.get(['/dashboard-pink', '/dashboard-blue'], (req, res) => {
     res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
